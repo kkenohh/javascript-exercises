8465ed35eb413ba40cdaa64003a9987db2706944
Ken Oh <kenoh17@uw.edu>
2022-08-31 23:36:05 -0700

    up to fib

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    str = str.toLowerCase().split(' ').join('');
+    str = str.split('').filter(c => c.charCodeAt(0) > 96 && c.charCodeAt(0) < 123).join('');
+    for (let i = 0; i < str.length / 2; i++) {
+        if (str[i] !== str[str.length - i - 1]) {
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
