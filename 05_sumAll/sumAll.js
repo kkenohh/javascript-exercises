36d8cf10c1db0129b64cb56630fed8eba472e170
Ken Oh <kenoh17@uw.edu>
2022-08-31 21:40:39 -0700

    did first few exercises

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,11 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    if (typeof a != 'number' ||
+        typeof b != 'number' ||
+        a < 0 ||
+        b < 0) return 'ERROR';
+    let arr = [a, b].sort((a, b) => a - b);
+    for (let i = arr[0] + 1; i <= arr[1]; i++) arr[0]+=i;
+    return arr[0];
 };
 
 // Do not edit below this line
